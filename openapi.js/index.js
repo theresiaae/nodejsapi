fb5e294b4dc70d74f21b211532f9ef6b2591929f
theresiaae <theresia.eleonora@gmail.com>
2025-03-21 22:56:05 +0800

    first commit

---
+++ openapi.js/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mysql from 'mysql2';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
+
+const swaggerDocument = YAML.parse(fs.readFileSync('./openapi/spec.yml', 'utf8'));
+
+const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: "" });
+const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM user', (err, results) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
